ef216f7f0933c9f5bcaf68e4b7b430ccac80f37a
Chlod Alejandro <chlod@chlod.net>
2024-05-31 23:20:09 +0800

    make build script compatible with windows

--- build.py
+++ build.py
@@ -11,13 +11,15 @@ def process(*args):
 
 def main():
     root = os.path.join(os.path.dirname(__file__), "static")
+    npx = "npx.cmd" if os.name == "nt" else "npx"
+
     for dirpath, dirnames, filenames in os.walk(root):
         for filename in filenames:
             name = os.path.relpath(os.path.join(dirpath, filename))
             if filename.endswith(".js") and ".min." not in filename:
-                process("uglifyjs", "--compress", "-o", name.replace(".js", ".min.js"), "--", name)
+                process(npx, "uglifyjs", "--compress", "-o", name.replace(".js", ".min.js"), "--", name)
             if filename.endswith(".css") and ".min." not in filename:
-                process("postcss", "-u", "cssnano", "--no-map", name, "-o",
+                process(npx, "postcss", "-u", "cssnano", "--no-map", name, "-o",
                         name.replace(".css", ".min.css"))
 
 if __name__ == "__main__":
